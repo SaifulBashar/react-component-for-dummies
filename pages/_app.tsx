6dba7763b67899fa336c6dc048a16401d0612048
saiful.bashar <robal.bashar59@gmail.com>
2021-09-25 12:18:51 +0600

    design front page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,21 @@
+import { Global, css } from "@emotion/react";
+
+const globalStyles = (
+  <Global
+    styles={css`
+      @import url("https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap");
+      html,
+      body {
+        margin: 0;
+        background: #e0e7ff;
+        min-height: 100vh;
+      }
+    `}
+  />
+);
 const App = ({ Component, pageProps }: any) => (
   <>
+    {globalStyles}
     <Component {...pageProps} />
   </>
 );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,41 @@
 import { Modal } from "../components/Modal";
 import { useState } from "react";
+import { css } from "@emotion/css";
 
 function Page() {
   const [open, setOpen] = useState(false);
   return (
-    <div>
-      <div>
-        <button onClick={() => setOpen(true)}>open modal</button>
-        <Modal isOpen={open} onClose={() => setOpen(false)} />
+    <div
+      className={css`
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+      `}
+    >
+      <h1
+        className={css`
+          font-family: "Press Start 2P", cursive;
+          font-size: 2rem;
+        `}
+      >
+        React Components for Dummies
+      </h1>
+      <div
+        className={css`
+          padding: 1rem;
+          margin: 1rem;
+          border-radius: 0.5rem;
+          max-width: 900px;
+          width: 100%;
+          box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+            0 2px 4px -1px rgba(0, 0, 0, 0.06);
+          background: white;
+        `}
+      >
+        <div>
+          <button onClick={() => setOpen(true)}>open modal</button>
+          <Modal isOpen={open} onClose={() => setOpen(false)} />
+        </div>
       </div>
     </div>
   );
