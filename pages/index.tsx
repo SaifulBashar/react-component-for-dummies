7a0c19ef513d42b5217c0657d8833be364120a08
saiful_bashar <saiful.bashar@evaly.com.bd>
2021-11-02 14:30:19 +0600

    header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,92 +7,68 @@ import { PopoverSection } from "../components/sections/PopoverSection";
 
 function Page() {
   return (
-    <>
-      <div
-        className={css`
-          display: flex;
-          align-items: center;
-          flex-direction: column;
-          background: linear-gradient(
-            to top,
-            #0f2027,
-            #203a43,
-            #2c5364
-          ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-        `}
-      >
-        <h1
-          className={css`
-            font-family: "Press Start 2P", cursive;
-            font-size: 2rem;
-            color: wheat;
-          `}
-        >
+    <div className="bg-gray-100">
+      <div className="bg-white shadow mb-8">
+        <h1 className=" text-center text-3xl font-bold p-4">
           React Components for Dummies
         </h1>
+      </div>
 
-        <div
-          className={css`
-            padding: 1rem;
-            display: flex;
-            border-radius: 0.5rem;
-            max-width: 1100px;
-            width: 100%;
+      <div
+        className={css`
+          padding: 1rem;
+          display: flex;
+          border-radius: 0.5rem;
+          max-width: 1100px;
+          width: 100%;
 
-            background: white;
-          `}
-        >
-          <div
+          background: white;
+        `}
+      >
+        <div className="fixed left-0">
+          <h4>Components</h4>
+          <ol
             className={css`
-              padding-right: 12px;
-              margin-right: 12px;
-              min-width: 180px;
+              list-style: none;
+              margin: 0;
+              padding: 0;
+              width: 100%;
+              li {
+                margin-top: 12px;
+                margin-bottom: 12px;
+              }
             `}
           >
-            <h4>Components</h4>
-            <ol
-              className={css`
-                list-style: none;
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                li {
-                  margin-top: 12px;
-                  margin-bottom: 12px;
-                }
-              `}
-            >
-              <li>
-                <a href="#modal-section">Modal</a>
-              </li>
-              <li>
-                <a href="#radio-section">Radio</a>
-              </li>{" "}
-              <li>
-                <a href="#pagination-section">Pagination</a>
-              </li>
-              <li>
-                <a href="#empty-section">Empty View</a>
-              </li>
-              <li>
-                <a href="#popover-section">Popover</a>
-              </li>
-            </ol>
-          </div>
-          <div>
-            <ModalSection />
-            <br />
-            <RadioSection />
-            <br />
-            <PaginationSection />
-            <br />
-            <EmptyViewSection />
-            <br />
-            <PopoverSection />
-          </div>
+            <li>
+              <a href="#modal-section">Modal</a>
+            </li>
+            <li>
+              <a href="#radio-section">Radio</a>
+            </li>{" "}
+            <li>
+              <a href="#pagination-section">Pagination</a>
+            </li>
+            <li>
+              <a href="#empty-section">Empty View</a>
+            </li>
+            <li>
+              <a href="#popover-section">Popover</a>
+            </li>
+          </ol>
+        </div>
+        <div>
+          <ModalSection />
+          <br />
+          <RadioSection />
+          <br />
+          <PaginationSection />
+          <br />
+          <EmptyViewSection />
+          <br />
+          <PopoverSection />
         </div>
       </div>
-    </>
+    </div>
   );
 }
 export default Page;
